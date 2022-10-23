af2d85e4abe28b609c4323d4b59a4fede4fdbbf3
shaky0907 <divad0907@gmail.com>
2022-10-22 21:13:04 -0600

    Initial commit

---
+++ DetailTECMovilApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DetailTECMovilApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new LoginUI();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

4ac807ed64804a604e18523fb8ecc4b1f99b9573
shaky0907 <divad0907@gmail.com>
2022-10-23 23:53:40 -0600

    Added Flyout

---
+++ DetailTECMovilApp/View/FlyoutItemPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DetailTECMovilApp
+{
+    internal class FlyoutItemPage
+    {
+        public string Title { get; set; }
+        public string IconSource {get; set; }
+
+        public Type TargetPage { get; set; }
+            
+    }
+}
---
+++ DetailTECMovilApp/View/LoginUI.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DetailTECMovilApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Login(object sender, EventArgs e)
+        {
+            if(txtUsername.Text=="admin" && txtPassword.Text == "0907")
+            {
+                Application.Current.MainPage = new Home();
+            }
+            else
+            {
+                DisplayAlert("Error","No se ingreso correctamente la información","Ok");
+            }
+        }
+    }
+}
